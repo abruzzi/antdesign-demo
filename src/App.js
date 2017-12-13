64cd54299ffaa20afe6de9860cd75aececdc169f
Juntao Qiu <juntao.qiu@gmail.com>
2017-12-13 22:22:45 +0800

    a simple layout demo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import { Button } from 'antd';
 import './App.css';
 
-class App extends Component {
+import { Row, Col } from 'antd';
+
+import SearchTree from './Tree'
+import {renderTable} from './Table'
+
+class App extends React.Component {
+
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
+      <div className="container">
+        <Row gutter={16}>
+          <Col span={8}>
+            <div className="gutter-box panel">
+              <SearchTree />
+            </div>
+          </Col>
+          <Col span={16}>
+            <div className="gutter-box panel">
+              {renderTable()}
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
